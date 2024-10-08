29cd7efb222468726a098e732c68bf7e2973a721
Miltontito <miltonhernanjosuegomez@gmail.com>
2024-10-08 13:51:37 -0300

    Se agregan unos comentarios al backend

--- Backend/api-base/src/main/java/com/untdf/flexinventory/base/Access/AccessInventory.java
+++ Backend/api-base/src/main/java/com/untdf/flexinventory/base/Access/AccessInventory.java
@@ -4,6 +4,13 @@ import com.untdf.flexinventory.base.Model.inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * En este ejemplo podemos ver como la interfaz del Modelo está vacia!!!
+ * <p>
+ * Esto es así ya que al extender de JpaRepository obtenemos toda la funcionalidad básica del manejo de entidades.
+ * @Author Milton Gómez.
+ */
+
 @Repository
 public interface AccessInventory extends JpaRepository<inventory, Integer> {
 
--- Backend/api-base/src/main/java/com/untdf/flexinventory/base/Service/ServiceInventory.java
+++ Backend/api-base/src/main/java/com/untdf/flexinventory/base/Service/ServiceInventory.java
@@ -8,6 +8,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Aqui vemos como a través de la etiqueta @Autowired se relaciona la clase <code>ServiceInventory</code> con <code>AccessInventory</code>
+ * sin necesidad de instanciar una clase, esto es parte del patrón "Injection Dependency", esto se cumple para el transformador también.
+ * <p>
+ * Podemos ver también como al hacer uso de access este provee el metodo findAll() el cual es provisto por JpaRepository.
+ * @Author Milton Gómez.
+ */
+
 @Service
 public class ServiceInventory {
 
