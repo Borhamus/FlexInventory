ee832a2b6945963b3a5e56d703f01ac54d3d9e9b
Xesderte <sardinamc1514@gmail.com>
2024-11-07 22:03:39 -0300

    First Footer

---
+++ Frontend/react-frontend/src/api-base/components/Footter.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { Panel } from 'primereact/panel';
+
+function Footter() {
+  return (
+<Panel headerTemplate={"FLEXINVETORY"} footerTemplate={"Header2"} toggleable>
+    <p className="m-0">
+    </p>
+</Panel>
+  )
+}
+
+export default Footter;
