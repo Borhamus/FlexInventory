1b50db9ad672291efacb084144042e84fa3de8df
Benjamin Gallarreta <benjamingallarreta@gmail.com>
2024-12-02 12:53:41 -0300

    pendiente - creacion de item

--- Backend/api-base/src/main/java/com/untdf/flexinventory/base/Transferable/TransferableItemCreate.java
+++ Backend/api-base/src/main/java/com/untdf/flexinventory/base/Transferable/TransferableItemCreate.java
@@ -1,15 +1,18 @@
 package com.untdf.flexinventory.base.Transferable;
 
+import java.util.Date;
 import java.util.List;
 
 public class TransferableItemCreate
 {
     private String name;
-    private List<Integer> attributes;
     private Integer inventory;
+    private Date creation_date;
+    private List<Integer> attributes;
     private List<Integer> catalogs;
 
     //<--- Setter y getters--->
+
 
     public String getName() {
         return name;
@@ -19,20 +22,28 @@ public class TransferableItemCreate
         this.name = name;
     }
 
-    public List<Integer> getAttributes() {
-        return attributes;
-    }
-
-    public void setAttributes(List<Integer> attributes) {
-        this.attributes = attributes;
-    }
-
     public Integer getInventory() {
         return inventory;
     }
 
     public void setInventory(Integer inventory) {
         this.inventory = inventory;
+    }
+
+    public Date getCreation_date() {
+        return creation_date;
+    }
+
+    public void setCreation_date(Date creation_date) {
+        this.creation_date = creation_date;
+    }
+
+    public List<Integer> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(List<Integer> attributes) {
+        this.attributes = attributes;
     }
 
     public List<Integer> getCatalogs() {
