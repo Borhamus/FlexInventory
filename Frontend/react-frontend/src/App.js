56492b837b841760f4b3fdf1e05f2404d7002dff
Miltontito <miltonhernanjosuegomez@gmail.com>
2025-04-29 11:22:44 -0300

    Cambios añadidos al menu lateral, se agrega un ejemplo de template

--- Frontend/react-frontend/src/App.js
+++ Frontend/react-frontend/src/App.js
@@ -1,9 +1,10 @@
     import './App.css';
 
-    import "primereact/resources/themes/rhea/theme.css";
+    import "primereact/resources/themes/md-light-deeppurple/theme.css";
     import "primereact/resources/primereact.min.css";
     import "primeicons/primeicons.css";
     import "/node_modules/primeflex/primeflex.css";
+    import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 
     import React from 'react';
     import AppRoutes from './routes/AppRoutes';
@@ -12,31 +13,33 @@
 
     function App() {
         return (
-            <div className="App">
-                
-                {/* ------| GRID SUPERIOR - NAVBAR Y USER SESSION |------ */}
-                <div class= "grid">
-                    <div class = "col-12">
-                        <Navbar />
-                    </div>
-                    {/* Coloca UserSession aquí, en el flujo normal de la página   
-                    <div class="col-2">
-                        <UserSession 
-                            userName="Usuario Ejemplo"
-                            userAvatar="/path/to/avatar.jpg"
-                            onLogout={() => alert("Logout")}
-                        />
-                    </div>
-                    */}  
-                </div>
-
-                {/* ------| GRID DE CONTENIDO - SE CARGA SEGÚN EL APPROUTES |------ */}
-                <div class = "grid">
-                        <div class = "col">
-                            <AppRoutes />
+            <PrimeReactProvider>
+                <div className="App">
+                    
+                    {/* ------| GRID SUPERIOR - NAVBAR Y USER SESSION |------ */}
+                    <div class= "grid">
+                        <div class = "col-12">
+                            <Navbar />
                         </div>
+                        {/* Coloca UserSession aquí, en el flujo normal de la página   
+                        <div class="col-2">
+                            <UserSession 
+                                userName="Usuario Ejemplo"
+                                userAvatar="/path/to/avatar.jpg"
+                                onLogout={() => alert("Logout")}
+                            />
+                        </div>
+                        */}  
+                    </div>
+
+                    {/* ------| GRID DE CONTENIDO - SE CARGA SEGÚN EL APPROUTES |------ */}
+                    <div class = "grid">
+                            <div class = "col">
+                                <AppRoutes />
+                            </div>
+                    </div>
                 </div>
-            </div>
+            </PrimeReactProvider>
         );
     }
 
--- Frontend/react-frontend/src/components/MenuLateral.js
+++ Frontend/react-frontend/src/components/MenuLateral.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import { Menu } from 'primereact/menu';
 
+
+
 function MenuLateral({ elementos, onElementoSeleccionado, onCrearInventario }) {
+    
+    // Template para los Elementos del menu lateral.
+    // Hacer un componente item renderer
+    const style = {height: "45px",display: "flex", marginLeft: "10px"};
+    const itemRenderer = (item) => (
+        
+        <div className='p-menuitem-content' class = "text-white" style={style}>
+            <a className="flex align-items-center p-menuitem-link">
+                <span className={item.icon} />
+                <span className="mx-2">{item.label}</span>
+                {item.shortcut && <span className="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{item.shortcut}</span>}
+            </a>
+        </div>
+    );
+    
     const items = [
         {
             label: 'Inventarios',
@@ -9,22 +26,26 @@ function MenuLateral({ elementos, onElementoSeleccionado, onCrearInventario }) {
                 ...elementos.map((elemento) => ({
                     label: elemento.name,
                     icon: 'pi pi-table',
-                    command: () => onElementoSeleccionado(elemento.id)
+                    command: () => onElementoSeleccionado(elemento.id),
+                    template: itemRenderer
                 })),
+
                 {
                     label: 'Crear Inventario',
                     icon: 'pi pi-plus',
                     command: onCrearInventario, // Llamamos la función pasada para abrir el modal
-                    style: { backgroundColor: '#28a745', color: 'white' },
-                    disabled: false
+                    style: { backgroundColor: '#28a745'},
+                    disabled: false,
+                    template: itemRenderer
                 },
 
                 {
                     label: 'Borrar Inventario',
                     icon: 'pi pi-minus',
                     command: onCrearInventario, // Llamamos la función pasada para abrir el modal
-                    style: { backgroundColor: 'red', color: "white" },
-                    disabled: false
+                    style: { backgroundColor: 'red', $textColor: "white" },
+                    disabled: false,
+                    template: itemRenderer
                 }
             ]
         }
