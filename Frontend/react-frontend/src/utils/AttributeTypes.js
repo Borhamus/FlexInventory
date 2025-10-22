484de2fe9e6b5b985dd945b3da76f47927bf0ccc
Milton Gomez <miltonhernanjosuegomez@gmail.com>
2025-10-21 23:53:46 -0300

    Se añaden las primeras constantes para los tipos de atributo

---
+++ Frontend/react-frontend/src/utils/AttributeTypes.js
@@ -0,0 +1,10 @@
+// src/constants/AttributeTypes.js
+export const AttributeTypes = {
+  NUMERO: "NUMERO",
+  TEXTO: "TEXTO",
+  FECHA: "FECHA",
+  BOOLEANO: "BOOLEANO",
+  SELECCION: "SELECCION",
+};
+
+export default Object.values(AttributeTypes);
